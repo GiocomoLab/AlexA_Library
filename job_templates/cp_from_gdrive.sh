04390485cc258682de396444aaf0cb18d582cf96
alex-attinger <a.attinger@gmail.com>
2019-01-31 18:10:00 -0800

    Update cp_from_gdrive.sh

--- job_templates/cp_from_gdrive.sh
+++ job_templates/cp_from_gdrive.sh
@@ -5,19 +5,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=attialex@stanford.edu
-#SBATCH --begin=now
-#SBATCH --dependency=singleton
-#SBATCH --signal=B:SIGUSR1@90
-# catch the SIGUSR1 signal
-_requeue() {
-    echo "$(date): job $SLURM_JOBID received SIGUSR1, re-queueing"
-    scontrol requeue $SLURM_JOBID
-}
-trap '_requeue' SIGUSR1
-module load system rclone
+#SBATCH --mem 100 # memory pool for all cores
+
 echo "$(date): job $SLURM_JOBID starting on $SLURM_NODELIST"
-rclone copy rclone copy VRGDrive:attialex /oak/stanford/groups/giocomo/attialex/DATA 
-wait
-sleep 1m
-date
-sleep 1m
+rclone copy rclone copy VRGDrive:attialex /oak/stanford/groups/giocomo/attialex/DATA -P
