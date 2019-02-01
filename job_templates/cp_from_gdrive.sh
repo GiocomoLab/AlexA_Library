ef811042e5254b3037502f028d4f6b478660e9c9
alex-attinger <a.attinger@gmail.com>
2019-01-31 18:13:07 -0800

    Update cp_from_gdrive.sh

--- job_templates/cp_from_gdrive.sh
+++ job_templates/cp_from_gdrive.sh
@@ -7,5 +7,6 @@
 #SBATCH --mail-user=attialex@stanford.edu
 #SBATCH --mem 100 # memory pool for all cores
 
+module load system rclone
 echo "$(date): job $SLURM_JOBID starting on $SLURM_NODELIST"
 rclone copy rclone copy VRGDrive:attialex /oak/stanford/groups/giocomo/attialex/DATA -P
