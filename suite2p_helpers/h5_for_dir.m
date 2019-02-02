fb53865a2d61816a5e51e6ee248cb43fac642a54
alex-attinger <a.attinger@gmail.com>
2019-02-01 16:06:27 -0800

    Update h5_for_dir.m

--- suite2p_helpers/h5_for_dir.m
+++ suite2p_helpers/h5_for_dir.m
@@ -7,7 +7,8 @@ for ii=1:length(sbx_files)
     [~,fn,~]=fileparts(sbx_files(ii).name);
     if ~exist([fn, '.h5'])
         sbx2h5(fullfile(input_dir,sbx_files(ii).name))
-        clearvars
+        fprintf('done /n')
+        clearvars info
     else
     fprintf([fn, '.h5 alrady exisits, skipping'])
     end
