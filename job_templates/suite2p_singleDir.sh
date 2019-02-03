0d3e8e6322e0cf90b449fa5fbf08feeeb101dc08
Alexander Attinger <attialex@sh-ln07.stanford.edu>
2019-02-03 13:32:40 -0800

    batch job on registration, still way too slow!

--- job_templates/suite2p_singleDir.sh
+++ job_templates/suite2p_singleDir.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=suite2p
-#SBATCH -p giocomo
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=attialex@stanford.edu
-#SBATCH -o /scratch/users/attialex/slurm.%N.%j.out # STDOUT
-#SBATCH -e /scratch/users/attialex/slurm.%N.%j.err # STDERR
+#SBATCH -o /scratch/users/attialex/register_jobs/slurm.%N.%j.out # STDOUT
+#SBATCH -e /scratch/users/attialex/register_jobs/slurm.%N.%j.err # STDERR
 #SBATCH --time=120:00
 #SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=20G
+#SBATCH --mem-per-cpu=30G
 
-echo "Hello World suite2p"
 ml python/3.6.1
 
-python3 $HOME/AlexA_Library/suite2p_helpers/run_single_file.py
+python3 $HOME/AlexA_Library/suite2p_helpers/run_single_file.py $OAK/attialex/DATA AA_190111_026
--- suite2p_helpers/register_DATA.py
+++ suite2p_helpers/register_DATA.py
@@ -6,35 +6,39 @@ def mkdir_p(dir):
         os.mkdir(dir)
     
 
-job_directory = "%s/.job" %os.environ['SCRATCH']
+job_directory = "%s/register_jobs" %os.environ['SCRATCH']
 scratch = os.environ['SCRATCH']
-data_dir = os.path.join(scratch, '/registration/')
+#data_dir = os.path.join(scratch, '/registration/')
 
 # Make top level directories
 mkdir_p(job_directory)
-mkdir_p(data_dir)
+#mkdir_p(data_dir)
 
-mice=["AA_190110_022"]
+
+mice=["AA_190110_022", "AA_190111_026","AA_190111_027","AA_190111_029"]
 root_dir=os.path.join(os.environ['OAK'],'attialex','DATA')
 for mouse in mice:
-
+    log_dir =os.path.join(job_directory,mouse)
+    mkdir_p(log_dir)
     job_file = os.path.join(job_directory,"%s.job" %mouse)
     #mouse_data = os.path.join(data_dir, mouse)
 
     # Create mouse directories
     #mkdir_p(mouse_data)
 
-    with open(job_file) as fh:
+    with open(job_file,'w') as fh:
         fh.writelines("#!/bin/bash\n")
+        fh.writelines("#SBATCH -p giocomo\n")
+        fh.writelines("#SBATCH --ntasks-per-node=1\n")
+        fh.writelines("#SBATCH --cpus-per-task=4\n")
         fh.writelines("#SBATCH --job-name=%s.job\n" % mouse)
-        fh.writelines("#SBATCH --output=.out/%s.out\n" % mouse)
-        fh.writelines("#SBATCH --error=.out/%s.err\n" % mouse)
-        fh.writelines("#SBATCH --time=4:00\n")
-        fh.writelines("#SBATCH --mem=1\n")
-        fh.writelines("#SBATCH --qos=normal\n")
-        fh.writelines("#SBATCH --mail-type=ALL\n")
-        fh.writelines("#SBATCH --mail-user=$USER@stanford.edu\n")
-        fh.writelines("ml python/3.6.1")
+        fh.writelines("#SBATCH -o "+log_dir+"/slurm.%N.%j.out\n")
+        fh.writelines("#SBATCH -e " + log_dir+"/slurm.%N.%j.err\n")
+        fh.writelines("#SBATCH --time=12:00:00\n")
+        fh.writelines("#SBATCH --mem-per-cpu=40G\n")
+        fh.writelines("#SBATCH --mail-type=BEGIN,END,FAIL\n")
+        fh.writelines("#SBATCH --mail-user=attialex@stanford.edu\n")
+        fh.writelines("ml python/3.6.1\n")
         fh.writelines("python3 $HOME/AlexA_Library/suite2p_helpers/run_single_file.py " + root_dir +' ' + mouse)
 
-    #os.system("sbatch %s" %job_file)
+    os.system("sbatch %s" %job_file)
