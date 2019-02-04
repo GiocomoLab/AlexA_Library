99a082b00a2b7102be38ba565150300510695127
Alexander Attinger <attialex@sh-ln02.stanford.edu>
2019-02-04 14:24:01 -0800

    updates

--- job_templates/generate_hd5.sh
+++ job_templates/generate_hd5.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=attialex@stanford.edu
-#SBATCH --mem-per-cpu=40G
+#SBATCH --mem-per-cpu=80G
 #SBATCH -o /scratch/users/attialex/slurm.%N.%j.out # STDOUT
 #SBATCH -e /scratch/users/attialex/slurm.%N.%j.err # STDERR
 
