948d31ec29e727244b431fa73219084fc5a6d5fd
alex-attinger <a.attinger@gmail.com>
2019-02-14 11:22:21 -0800

    adding copied functions

--- correlation/example.m
+++ correlation/example.m
@@ -1,3 +1,6 @@
 good_cells = sp.cids(sp.cgs==2);
 idx=ismember(sp.clu,good_cells):
 [a,b]=CCG(sp.st(idx),double(sp.clu(idx)),'binSize',[0.001],'duration',[0.5]);
+
+mex -O CCGHeart.c
+bz_MonoSynConvClick(double(tempSP),sp.st(idx))
