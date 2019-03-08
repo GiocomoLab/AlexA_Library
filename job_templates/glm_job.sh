a9a09f3dd0ff0457eebc29e1dfff4d9ff834476c
attialex <attialex@NEUROB-C02WW00XHV2R.local>
2019-03-08 11:18:44 -0800

    job file for glm

---
+++ job_templates/glm_job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -p owners
+#SBATCH --job-name=xcorr_connect
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=attialex@stanford.edu
+#SBATCH --mem-per-cpu=40G
+#SBATCH --time=4:00:00
+#SBATCH -o /scratch/users/attialex/glm.%N.%j.out # STDOUT
+#SBATCH -e /scratch/users/attialex/gml.%N.%j.err # STDERR
+
+module load matlab
+echo "$(date): job $SLURM_JOBID starting on $SLURM_NODELIST"
+
+matlab -nodisplay -nosplash -r "run $HOME/AlexA_Library/spline-lnp-model-VR/glmModel_dir.m, exit"
