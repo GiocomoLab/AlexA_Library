a14ff479f3282926353ffc1618b799b4d54a9a0c
alex-attinger <a.attinger@gmail.com>
2019-03-12 13:01:40 -0700

    Update connectivity_dir.m
    adding/subtracting 1 from clu bc it won't work otherwise

--- correlation/connectivity_dir.m
+++ correlation/connectivity_dir.m
@@ -6,20 +6,21 @@ for iF=1:length(Files)
     load(fullfile(root,Files(iF).name),'connected');
     %if ~exist('connected','var')
     try
-    fprintf('Now working on %s \n',Files(iF).name )
-    load(fullfile(root,Files(iF).name));
-    good_cells = sp.cids(sp.cgs==2);
-    idx=ismember(sp.clu,good_cells);
-
-    spikes = double(sp.clu(idx))+1; %add 1 bc it fails for cluID ==0
-    tempSP=[ones(size(spikes)) spikes spikes];
-    mono=bz_MonoSynConvClick(double(tempSP),sp.st(idx),'plot',false);
-    connected = mono.sig_con; %subtract 1 to make connected correspond to cluid
-    fprintf('Done for %s, now saving. \n',Files(iF).name)
-    save(fullfile(root,Files(iF).name),'connected','-append')
+        fprintf('Now working on %s \n',Files(iF).name )
+        load(fullfile(root,Files(iF).name));
+        good_cells = sp.cids(sp.cgs==2);
+        idx=ismember(sp.clu,good_cells);
+        
+        spikes = double(sp.clu(idx))+1; %add 1 bc it fails for cluID ==0
+        tempSP=[ones(size(spikes)) spikes spikes];
+        mono=bz_MonoSynConvClick(double(tempSP),sp.st(idx),'plot',false);
+        connected = mono.sig_con;
+        connected = connected -1;%subtract 1 to make connected correspond to cluid
+        fprintf('Done for %s, now saving. \n',Files(iF).name)
+        save(fullfile(root,Files(iF).name),'connected','-append')
     catch ME
-    fprintf('Failed for %s \n',Files(iF).name)
-    warning(ME.message)
+        fprintf('Failed for %s \n',Files(iF).name)
+        warning(ME.message)
     end
     %end
 end
