584d7ee01b2d005e9ea155f2e237d9dfc70cfb3f
alex-attinger <a.attinger@gmail.com>
2019-03-14 10:02:53 -0700

    copied Car2Dat and created NL2binscript

---
+++ NeuroPixel/convertNL2binScript.m
@@ -0,0 +1,6 @@
+addpath(genpath('Y:\giocomo\export\data\Projects\ContrastExperiment_neuropixels\Analysis'))
+%%
+sourcedir='Y:\giocomo\export\data\Projects\ContrastExperiment_neuropixels\H1\CambridgeNeurotech\2019-03-11_12-23-58';
+targetdir = 'F:\H1\2019-03-11_12-23-58';
+
+neuralynx2kilosort(sourcedir,targetdir)
