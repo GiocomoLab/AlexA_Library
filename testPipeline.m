bb072e3906714fc3b7592763482c4cd743fefc5a
alex-attinger <a.attinger@gmail.com>
2019-04-23 13:55:46 -0700

    Delete testPipeline.m

--- testPipeline.m
+++
@@ -1,9 +0,0 @@
-%load spike structure with spike times
-
-% calculate firing rate
-
-% downsample to VR time
-
-% do some triggered averaging
-
-% for some interesting cells, extract single spikes for triggered times
