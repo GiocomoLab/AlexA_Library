d7bb29880c7cef027462c4a8b5316b816b6c22cb
alex-attinger <a.attinger@gmail.com>
2019-05-29 18:14:04 -0700

    extract and plot lfp

---
+++ LFP/extract_plotLFP.m
@@ -0,0 +1,45 @@
+myKsDir = 'F:\J1\npJ1_0520_baseline_g0\npJ1_0520_baseline_g0_imec0';
+lfpD = dir(fullfile(myKsDir, '*.lf.bin')); % LFP file from spikeGLX specifically
+lfpFilename = fullfile(myKsDir, lfpD(1).name);
+
+lfpFs = 2500;  % neuropixels phase3a
+nChansInFile = 385;  % neuropixels phase3a, from spikeGLX
+
+nClips = 10;
+clipDur = 10; % seconds
+
+% load nClips one-sec samples
+d = dir(lfpFilename); 
+nSamps = d.bytes/2/nChansInFile;
+sampStarts = round(linspace(lfpFs*10, nSamps, nClips+1)); % skip first 10 secs
+nClipSamps = round(lfpFs*clipDur);
+
+
+
+mmf = memmapfile(lfpFilename, 'Format', {'int16', [nChansInFile nSamps], 'x'});
+n=1;
+thisDat = double(mmf.Data.x(:, (1:nClipSamps)+sampStarts(n)));
+thisDat = bsxfun(@minus, thisDat, mean(thisDat,2));
+%%
+L = size(thisDat',1);
+NFFT = 2^nextpow2(L);
+[Pxx,F] = pwelch(thisDat',[],[],NFFT,lfpFs);
+thetaPower = mean(Pxx(F>3 & F<13,:));
+[a,maxChan]=max(thetaPower);
+%%
+bp = bandpass(thisDat(maxChan,:),[8 13],2500);
+
+figure
+plot(thisDat(maxChan,:))
+hold on
+plot(bp)
+
+[pks,locs] = findpeaks(bp);
+plot(locs,bp(locs),'ro')
+
+snps=extract_snps(thisDat,locs,'win',[-200 200]);
+aa=squeeze(mean(snps(:,:,1:10),3));
+
+step = 4;
+col = parula(ceil(nChansInFile/step));
+figure;hold on;idx =1;for ii=1:step:nChansInFile;plot(aa(ii,:),'Color',col(idx,:));idx = idx+1;end;
---
+++ probe_helpers/array2stringCell.m
@@ -0,0 +1,3 @@
+function c_array = array2stringCell(array)
+c_array = cellstr(num2str(array));
+end
