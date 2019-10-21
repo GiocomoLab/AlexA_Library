2a6b65107cf12dc80639624cf6421c8069d88500
attialex <attialex@NEUROB-C02WW00XHV2R.local>
2019-10-21 17:00:35 -0500

    job for speed loop

---
+++ job_templates/speed_loop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -p owners
+#SBATCH --job-name=rigid_reg
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=attialex@stanford.edu
+#SBATCH --mem-per-cpu=70G
+#SBATCH --time=4:00:00
+#SBATCH -o /scratch/users/attialex/reg.%N.%j.out # STDOUT
+#SBATCH -e /scratch/users/attialex/reg.%N.%j.err # STDERR
+
+module load matlab
+echo "$(date): job $SLURM_JOBID starting on $SLURM_NODELIST"
+
+matlab -nodisplay -nosplash -r "run $HOME/AlexA_Library/NeuroPixel/speed_sorting/loop_files_speed.m, exit"
