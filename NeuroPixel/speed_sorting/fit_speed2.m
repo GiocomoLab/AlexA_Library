1fc805b962740fa526366e7b87f56d0dfe0abaf9
alex-attinger <a.attinger@gmail.com>
2019-10-31 10:19:18 -0700

    updating fit speed

--- NeuroPixel/speed_sorting/fit_speed2.m
+++ NeuroPixel/speed_sorting/fit_speed2.m
@@ -1,26 +1,36 @@
-function fit_speed2(filepath,savepath,params)
+function fit_speed2(filepath,savepath,params,im_save_root)
 [~,session_name,~]=fileparts(filepath);
 
+if ~isempty(im_save_root)
+    image_save_dir = fullfile(im_save_root,session_name);
+    if ~isfolder(image_save_dir)
+        mkdir(image_save_dir)
+    end
+    plot_data=true;
+else
+    plot_data=false;
+end
 
 %load file
-load(filepath);
-
+data=load(filepath);
+fn=fieldnames(data);
+for iF=1:numel(fn);eval(sprintf('%s = data.%s;',fn{iF},fn{iF}));end
 
 %process anatomy
 if ~isvarname('anatomy')
     return
 end
 try
-if isfield(anatomy,'parent_shifted')
-    region = anatomy.parent_shifted;
-else
-    region = anatomy.cluster_parent;
-end
+    if isfield(anatomy,'parent_shifted')
+        region = anatomy.parent_shifted;
+    else
+        region = anatomy.cluster_parent;
+    end
 catch
     disp('no anatomy')
     return
 end
-    
+
 %calculate spatial maps
 
 trials=[1:max(trial)];
@@ -112,25 +122,54 @@ data.clu_region = region;
 data.posWindow = posWindow;
 
 save(fullfile(savepath,session_name),'data')
+
+if plot_data
+    corr = nan(1,numel(delays));
+    for iR=1:numel(delays)
+        if ~isempty(delays{iR})
+            
+            bl_idx = delays{iR}(:,3)==1 & delays{iR}(:,5)==1 & delays{iR}(:,4)==100 & delays{iR}(:,6)==100;
+            tmp = corrcoef(delays{iR}(bl_idx,1),delays{iR}(bl_idx,2));
+            corr(iR)=tmp(1,2);
+        end
+    end
+end
+thresh = 0.08;
+if plot_data && any(abs(corr)>thresh)
+    [~,sid]=sort(trial_speed);
+    
+    trial_sorted =trial;
+    trial_rank = 1:max(trial);
+    trial_rank(sid)=trial_rank;
+    for ii=1:max(trial)
+        idx = trial==ii;
+        trial_sorted(idx)=trial_rank(ii);
+    end
+    data.region = region;
+    data.posWindow = posWindow;
+    data.trial_sorted = trial_sorted;
+    plotRasterSpeedSort(data,params,image_save_dir)
+end
+
 end
 
 function delay_list=calcXCorr(flatMap,trial_speed,trial_gain,trial_contrast)
 nTrials = size(flatMap,1);
 delay_list=zeros(nTrials*(nTrials-1)/2,6); %delay, speed_diff, %i gain, i contrast, jgain j contrast
-    idx = 0;
-    max_lag=20;
-    for iT = 1:nTrials
-        for jT=(iT+1):nTrials
-            idx = idx+1;
-            rr=xcorr(flatMap(iT,:),flatMap(jT,:),max_lag);
-            [~,midx]=max(rr);
-            if ismember(midx,[1 size(rr,2)])
-                midx = nan;
-            end
-            
-            
-            tmp_d = max_lag+1;
-            delay_list(idx,:)=[midx-tmp_d,trial_speed(iT)-trial_speed(jT), trial_gain(iT),trial_contrast(iT),trial_gain(jT),trial_contrast(jT) ];
+idx = 0;
+max_lag=20;
+for iT = 1:nTrials
+    for jT=(iT+1):nTrials
+        idx = idx+1;
+        rr=xcorr(flatMap(iT,:),flatMap(jT,:),max_lag);
+        [~,midx]=max(rr);
+        if ismember(midx,[1 size(rr,2)])
+            midx = nan;
         end
+        
+        
+        tmp_d = max_lag+1;
+        delay_list(idx,:)=[midx-tmp_d,trial_speed(iT)-trial_speed(jT), trial_gain(iT),trial_contrast(iT),trial_gain(jT),trial_contrast(jT) ];
     end
+end
 end
