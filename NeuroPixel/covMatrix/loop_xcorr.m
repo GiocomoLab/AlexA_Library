e4e05ba7de5b4f669712baf5fa8c161471092bad
alex-attinger <a.attinger@gmail.com>
2019-11-11 17:51:26 -0800

    Update loop_xcorr.m

--- NeuroPixel/covMatrix/loop_xcorr.m
+++ NeuroPixel/covMatrix/loop_xcorr.m
@@ -1,17 +1,27 @@
 chunksize=50;
 stride = 20;
 files = dir('F:\NP_DATA\npF4*_gain*.mat');
+trials = [11:34];
+PEAKS=zeros(24,8,numel(files));
+SHIFTS = PEAKS;
 for iF = 1:numel(files)
     data = load(fullfile(files(iF).folder,files(iF).name));
-    [PEAKS,SHIFTS]=calculatePeakShiftSession(data,trials,chunksize,stride);
+    [peak,shift]=calculatePeakShiftSession(data,trials,chunksize,stride);
+    PEAKS(:,:,iF)=peaks;
+    SHIFTS(:,:,iF)=shifts;
+    startVec = 1:stride:(200-chunksize);
+    
      x=startVec+25;
     x = x-1;
     x = x*2;
-    figure
+    fig = figure();
     hold on
 for iT = 1:24
     tmp = x+400*(iT-1);
-    plot(tmp,PEAKS(iT,:),'b.')
+    plot(tmp,peak(iT,:),'b.')
 end
-    saveas(fig,fullfile('/oak/stanford/groups/giocomo/attialex/Images/xcorrv1',[session_name +'.png']))
+    drawnow
+    session_name = files(iF).name(1:end-4);
+    %saveas(fig,fullfile('/oak/stanford/groups/giocomo/attialex/Images/xcorrv1',[session_name +'.png']))
+    %close(fig)
 end
