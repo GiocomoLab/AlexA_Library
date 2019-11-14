11df5a03b526603ad6e82e37188382c808e951b3
attialex <attialex@NEUROB-C02WW00XHV2R.local>
2019-11-14 07:59:05 -0800

    increased flexibility for selection criteria

--- NeuroPixel/getFilesCriteria.m
+++ NeuroPixel/getFilesCriteria.m
@@ -7,9 +7,22 @@ for iF = 1:numel(files)
     data = load(fullfile(files(iF).folder,files(iF).name),'anatomy','trial_gain','trial_contrast');
     gaincontrastcombo = false;
     if isfield(data,'trial_gain') && isfield(data,'trial_contrast')
+        if gain_to_look_at == 1
+            %find condition with given contrast and no gain change
+            bin = data.trial_gain == gain_to_look_at & data.trial_contrast == contrast;
+            trigger_tmp = strfind(bin',[1 1 1 1 1 1 1 1 1 1 1 1]) +4;
+            trigger_tmp(trigger_tmp <10) = [];
+            gaincontrastcombo = nnz(trigger_tmp)>0;
+        elseif gain_to_look_at == 0
+            %special case: find where gain ==1 and for a given contrast
+            %change
+            gaincontrastcombo = any(data.trial_gain == 1 & data.trial_contrast == contrast);
+            trigger_tmp = strfind((data.trial_gain == 1 & data.trial_contrast == contrast)',[0 1])+1;
+        else
         gaincontrastcombo = any(data.trial_gain == gain_to_look_at & data.trial_contrast == contrast);
         trigger_tmp = strfind((data.trial_gain == gain_to_look_at & data.trial_contrast == contrast)',[0 1])+1;
-    end
+        end
+        end
     containsregion = false;
     if isfield(data,'anatomy')
         containsregion = any(startsWith(data.anatomy.cluster_parent,region));
