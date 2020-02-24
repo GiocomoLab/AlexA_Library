204297dbc4202d4985a817bca48653094563bcc6
alex-attinger <a.attinger@gmail.com>
2020-02-24 10:39:44 -0800

    writing out ops, only 2 xcorrs per traversal, different save path

--- NeuroPixel/covMatrix/loop_xcorr2.m
+++ NeuroPixel/covMatrix/loop_xcorr2.m
@@ -1,7 +1,7 @@
 ops.chunksize=100; %in bins,so thats 200 cm
-ops.stride_start = 10;
+ops.stride_start = 1;%10;
 ops.binsize=2;
-ops.stride = 10;
+ops.stride = 99;
 startVec = ops.stride_start:ops.stride:(400/ops.binsize-ops.chunksize+1);
 chunksPerTrials = numel(startVec);
 ops.region = 'MEC';
@@ -11,6 +11,7 @@ ops.tt=(-6:9);
 ops.template_trials = 1:6;
 ops.stability_threshold=0.2;
 ops.smoothSigma = 4;
+ops.startVec = startVec;
 %%
 [filenames,triggers] = getFilesCriteria(ops.region,ops.contrast,ops.gain_to_look_at,'/oak/stanford/groups/giocomo/attialex/NP_DATA');
 %[filenames,triggers] = getFilesCriteria(region,contrast,gain_to_look_at,'/users/attialex/Desktop/data');
@@ -22,12 +23,13 @@ if isempty(p)
     parpool(12);
 end
 %%
-savepath_root = '/oak/stanford/groups/giocomo/attialex/Images/xcorrv8';
+savepath_root = '/oak/stanford/groups/giocomo/attialex/images/xcorrv9';
 %savepath_root = '/users/attialex/tmp/';
 savepath = fullfile(savepath_root,sprintf('%s_%.2f_%d',ops.region,ops.gain_to_look_at,ops.contrast));
 if ~isfolder(savepath)
     mkdir(savepath)
 end
+save(fullfile(savepath,'params.mat'),'ops');
 
 %%
 n_chunks = 0;
@@ -44,6 +46,7 @@ for ii=1:numel(filenames)
         chunk_idx{ii}(iC)=n_chunks;
         loop_data(n_chunks).filename = filenames{ii};
         loop_data(n_chunks).trigger = triggers{ii}(iC);
+        loop_data(n_chunks).iRep = iC;
     end
 end
 
@@ -56,6 +59,7 @@ XTX = zeros(numel(ops.tt)*200,numel(ops.tt)*200,n_chunks);
 YYT = zeros(numel(ops.tt),numel(ops.tt),n_chunks);
 SPEED=zeros(numel(ops.tt),200,n_chunks);
 FR=SPEED;
+%%
 %cntr = 0;
 parfor iF = 1:n_chunks
 %     data = load(filenames{iF});
@@ -64,7 +68,9 @@ parfor iF = 1:n_chunks
 %         current_trig = triggers{iF}(iTrigger);
         data = load(loop_data(iF).filename);
         current_trig = loop_data(iF).trigger;
-        
+        [~,sn]=fileparts(loop_data(iF).filename);
+        rep = loop_data(iF).iRep;
+        data_out = matfile(fullfile(savepath,sprintf('%s_%d',sn,rep)),'Writable',true);
         trials = current_trig+ops.tt;
         peak = 0;
         shift = 0;
@@ -76,7 +82,9 @@ parfor iF = 1:n_chunks
             rethrow(ME)
         end
         [~,session_name,~] = fileparts(loop_data(iF).filename);
-
+        data_out.peak=peak;
+        data_out.shift = shift;
+        
         PEAKS(:,:,iF)=peak;
         SHIFTS(:,:,iF)=shift;
         MouseID{iF}=session_name;
@@ -120,11 +128,11 @@ end
 X=[];
 
 
-x=startVec+chunksize/2;
+x=startVec+ops.chunksize/2;
 x = x-1;
-x = x*binsize;
-for iT = 1:numel(tt)
-    tmp = x+400*(iT-1);
+x = x*ops.binsize;
+for iT = 1:numel(ops.tt)
+    tmp = x+400*(ops.tt(iT));
     X=cat(2,X,tmp);
     %plot(tmp,peak(iT,:),'b.')
 end
@@ -132,7 +140,7 @@ Y=zeros(size(PEAKS,3),numel(X));
 S = Y;
 chunks_per_trial = size(PEAKS,2);
 for iS = 1:size(PEAKS,3)
-    for iT = 1:numel(tt)
+    for iT = 1:numel(ops.tt)
         idx = ((iT-1)*chunks_per_trial+1):iT*chunks_per_trial;
         tmp = squeeze(PEAKS(iT,:,iS));
         Y(iS,idx)=tmp;
@@ -142,23 +150,20 @@ for iS = 1:size(PEAKS,3)
     end
 end
 
-output=struct();
+output = matfile(fullfile(savepath_root,sprintf('allData_%s_%.1f_%d.mat',ops.region,ops.gain_to_look_at,ops.contrast)),'Writable',true);
 output.ops = ops;
 output.X=X;
 output.Y = Y;
 output.S = S;
-output.region = region;
-output.gain = gain_to_look_at;
-output.contrast = contrast;
 output.loop_data = loop_data;
 output.NUnits = NUnits;
 output.XTX =nanmean(XTX,3);
 output.YYT = YYT;
 output.SPEED = SPEED;
 output.FR = FR;
-%figure
-%plot(output.X-4000,nanmean(output.Y))
-%hold on
-save(fullfile(savepath_root,sprintf('allData_%s_%.1f_%d.mat',region,gain_to_look_at,contrast)),'output','-v7.3')
+figure
+plot(output.X,nanmean(output.Y))
+hold on
+%save(fullfile(savepath_root,sprintf('allData_%s_%.1f_%d.mat',region,gain_to_look_at,contrast)),'output','-v7.3')
 %%
 
