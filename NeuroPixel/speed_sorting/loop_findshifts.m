be07a7e7d856ff6a7ce8403ec7a68d6fcc3819ab
alex-attinger <a.attinger@gmail.com>
2020-03-03 11:32:20 -0800

    updating to loop over multiple excerpts per session

--- NeuroPixel/speed_sorting/loop_findshifts.m
+++ NeuroPixel/speed_sorting/loop_findshifts.m
@@ -1,21 +1,22 @@
 
 
-ops.factors = -.25:0.01:.25;
-ops.edges = 0:2:400;
+ops.factors = -.55:0.01:.55;
+ops.BinWidth =2;
+ops.edges = 0:ops.BinWidth:400;
 ops.nBins = numel(ops.edges)-1;
-
-%ops.trials = find(data.trial_gain ==1 & data.trial_contrast==100);
-ops.trials = 3:20;
+ops.n_preceeding = 18;
 ops.TimeBin = 0.02;
-ops.idx = [10:2:390]/2;% in bins
+ops.idx = [10:ops.BinWidth:390]/ops.BinWidth;% in bins
 fi = gausswin(5);
 fi=fi'/sum(fi);
 ops.filter = fi;
-ops.plotfig = true;
+ops.n_trials = 10;
+ops.plotfig = false;
+ops.maxLag = 20; % in cm
 OAK='/oak/stanford/groups/giocomo/';
 %% savedir =
-%savedir = fullfile(OAK,'attialex','speed_filtered_showMap');
-savedir = fullfile('F:/temp/','speed_filtered');
+savedir = fullfile(OAK,'attialex','speed_filtered_5');
+%savedir = fullfile('F:/temp/','speed_filtered');
 imdir = fullfile(savedir,'images');
 if ~isfolder(savedir)
     mkdir(savedir);
@@ -48,38 +49,54 @@ region = 'VISp';
 %%
 p=gcp('nocreate');
 if isempty(p)
-    parpool(6);
+    parpool(12);
 end
 
 %%
 
 
-for iF=1:numel(filenames)
+parfor iF=1:numel(filenames)
     try
         data = load(filenames{iF});
         ops_temp = ops;
-        ops_temp.trials=triggers{iF}(iRep)+[-18:3];
-        test_trials = triggers{iF}(iRep)+[4:13];
-        %ops_here.trial = find(data.trial_gain ==1 & data.trial_contrast==100);
-        [data_out,fighandles] = findBestShifts(data,ops_temp);
-        [~,mi]=max(data_out.all_stability,[],2);
-        factors = ops_temp.factors(mi);
-        
-        
-        
-        [~,session_name,~]=fileparts(filenames{iF});
-        if ops.plotfig
-            for ifig = 1:numel(fighandles)
-                saveas(fighandles{ifig},fullfile(imdir,sprintf('%s_%d.png',session_name,ifig)))
-                close(fighandles{ifig})
+        bl_trials = 1:numel(data.trial_contrast);
+        bl_trials = bl_trials(data.trial_contrast==100 & data.trial_gain==1);
+        not_done = true;
+        start=1;
+        good_starts = [];
+        while not_done
+            t_vec = start:(start+ops.n_trials-1);
+            if all(ismember(t_vec,bl_trials))
+                good_starts(end+1)=start;
+                start=start+ops.n_trials;
+            else
+                start=start+1;
             end
+            if start>max(bl_trials)
+                not_done = false;
+            end
+            
         end
+        nC=nnz(data.sp.cgs==2);
+        all_factors = nan(numel(good_starts),nC);
+        %ops_here.trial = find(data.trial_gain ==1 & data.trial_contrast==100);
+        for iRep=1:3%numel(good_starts)
+            ops_temp.trials = good_starts(iRep)+[0:ops_temp.n_trials-1];
+            [data_out,fighandles] = findBestShifts(data,ops_temp);
+            [~,mi]=max(data_out.all_stability,[],2);
+            factors = ops_temp.factors(mi);
+            all_factors(iRep,:)=factors;
+        end
+        
+        [~,session_name]=fileparts(filenames{iF});
         mf =matfile(fullfile(savedir,session_name));
         mf.stability = data_out.all_stability;
         mf.region = data_out.region;
-        mf.test_trials = test_trials;
-        mf.correlation_noshift = correlation_noshift;
-        mf.correlation_shifted = correlation_shifted;
+        mf.subregion = data_out.sub_reg;
+        mf.depth = data_out.depth;
+        mf.CID = data_out.CID;
+        mf.start_idx = good_start;
+        mf.all_factors = all_factors;
     catch ME
         fprintf('%s \nFailed for %s: %d \n',ME.message,filenames{iF},iF)
     end
