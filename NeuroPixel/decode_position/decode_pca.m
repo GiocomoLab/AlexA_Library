f9b4432fb725001240a460b431715f3d21a180ac
attialex <attialex@NEUROB-C02WW00XHV2R.local>
2020-04-06 15:12:10 -0700

    adding a trajectory based classifier

---
+++ NeuroPixel/decode_position/decode_pca.m
@@ -0,0 +1,150 @@
+%% baseline decoder
+
+ops = load_default_opt;
+ops.xbin = 2;
+ops.xbinedges = 0:ops.xbin:400;
+ops.xbincent = ops.xbinedges(1:end-1)+ops.xbin/2;
+ops.nBins = numel(ops.xbincent);
+ops.edges = ops.xbinedges;
+ops.filter = gausswin(11);
+ops.trials=5:20;
+ops.num_pcs_decoding = 10;
+regions = {'MEC','VISp','RS'}
+%%
+data_path = '/Volumes/Samsung_T5/attialex/NP_Data/';
+savepath = '/oak/stanford/groups/giocomo/attialex/pca_classifier/';
+if ~isdir(savepath)
+    mkdir(savepath)
+end
+files = dir(fullfile(data_path,'*.mat'));
+animal_list = {};
+valid_files = {};
+for iF=1:numel(files)
+    [~,sn]=fileparts(files(iF).name);
+    
+    parts = strsplit(sn,'_');
+    animal = parts{1};
+    date = parts{2};
+    animal_date = [animal, '_',date];
+    if ~contains(sn,{'mismatch','playback','dark'}) && ~ismember(animal_date,animal_list)
+        valid_files{end+1}=files(iF).name;
+        animal_list{end+1}=animal_date;
+    end
+end
+%%
+p=gcp('nocreate');
+if isempty(p)
+    parpool(12);
+end
+
+%%
+parfor iF=1%:numel(valid_files)
+    data = load(fullfile(data_path,valid_files{iF}));
+    [~,sn]=fileparts(valid_files{iF});
+    if ~isfield(data,'anatomy')
+        continue
+    end
+    if isfield(data.anatomy,'parent_shifted')
+        reg = data.anatomy.parent_shifted;
+    else
+        reg = data.anatomy.cluster_parent;
+    end
+    if iscolumn(reg)
+        reg=reg';
+    end
+    
+    good_cells_idx = data.sp.cgs == 2 & startsWith(reg,regions);
+    if nnz(good_cells_idx)<2
+        continue
+    end
+    
+    tmp_reg = reg(good_cells_idx);
+    good_cells = data.sp.cids(good_cells_idx);
+    good_idx = ismember(data.sp.clu,good_cells);
+    clu_tmp = data.sp.clu(good_idx);
+    st_tmp = data.sp.st(good_idx);
+    
+    [a,~,clus]=unique(clu_tmp);
+    if isfield(data.anatomy,'depth')
+        depth = data.anatomy.depth(good_cells_idx);
+    else
+        if ~isnan(data.anatomy.z2)
+            depth = data.anatomy.tip_distance(good_cells_idx)-data.anatomy.z2;
+        else
+            depth = data.anatomy.tip_distance(good_cells_idx)-1000;
+        end
+    end
+    if ~isrow(depth)
+        depth=depth';
+    end
+    
+    nClu = numel(a);
+    ops_temp = ops;
+    ops_temp.trials = 1:max(data.trial);
+    [spMap,firingRate] = shiftAllMapsByFactor(ops_temp,clus,st_tmp,nClu,data.posx,data.post,data.trial,0,0);
+    maxlag=100;
+    XC=zeros(size(spMap,3),2*maxlag+1);
+    stab = nan(1,size(spMap,3));
+    for cellIDX =1:size(spMap,3)
+        t1=squeeze(spMap(5:20,:,cellIDX));
+        cc = corr(t1');
+        c_idx = triu(true(size(t1,1)),1);
+        stab(cellIDX) = nanmean(cc(c_idx));
+    end
+    
+    if nnz(stab>.5)<5
+        continue
+    end
+    
+    %calculate temporal firing rates
+    fr = calcFRVsTime(good_cells,data,ops,ops.trials);
+    
+    % threshold by running speed
+    speed = calcSpeed(data.posx,ops);
+    X = fr(:,speed>ops.SpeedCutoff);
+    trial = data.trial(speed>ops.SpeedCutoff);
+    posx = data.posx(speed>ops.SpeedCutoff);
+    speed = speed(speed>ops.SpeedCutoff);
+    
+    % normalize each cell to have FR between 0 and 1
+    X = (X-nanmin(X,[],2))./repmat(nanmax(X,[],2)-nanmin(X,[],2),1,size(X,2));
+    
+    % set nans to 0
+    X(isnan(X)) = 0;
+    
+    % mean subtract
+    X = X - nanmean(X,2);
+    
+    % perform PCA
+    coeffs = pca(X');
+    num_components = min(ops.num_pcs_decoding,size(coeffs,2)); % sometimes number of cells is < 10
+    Xtilde = coeffs(:,1:num_components)' * X;
+    [~,~,posbin] = histcounts(posx,ops.xbinedges);
+    posbin(posbin==0) = 1;
+    pred_pos=cell(1,numel(ops.trials));
+    true_pos = cell(1,numel(ops.trials));
+    for iFold = 1:numel(ops.trials)
+        take_idx = true(1,numel(ops.trials));
+        take_idx(iFold)=false;
+        %train classifier
+        train_trials = ops.trials(take_idx);
+         train_trial_idx=ismember(trial,train_trials);
+
+        test_trial_idx = trial==ops.trials(iFold);
+        
+        tc = nan(num_components,ops.nBins,1);
+        for i = 1:ops.nBins
+            tc(:,i) = mean(Xtilde(:,posbin==i & train_trial_idx),2);
+        end
+        
+        % decode position in gain change trials based on baseline1 trials
+        dot_prod = tc' * Xtilde(:,test_trial_idx); % predict position
+        [~,max_bin] = max(dot_prod);
+        pred_pos{iFold} = ops.xbincent(max_bin);
+        true_pos{iFold} = ops.xbincent(posbin);
+    end
+    mf = matfile(fullfile(savepath,sn));
+    mf.cluID = good_cells;
+    mf.pred_pos = pred_pos;
+    mf.true_pos = true_pos;
+end
