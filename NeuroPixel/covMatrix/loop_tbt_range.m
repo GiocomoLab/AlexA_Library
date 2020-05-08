aea780de9e4ef5fad0092c583d381acf99ad4ccc
attialex <attialex@NEUROB-C02WW00XHV2R.local>
2020-05-07 17:13:42 -0700

    adding plotting

--- NeuroPixel/covMatrix/loop_tbt_range.m
+++ NeuroPixel/covMatrix/loop_tbt_range.m
@@ -11,8 +11,9 @@ ops.max_lag = 30;
 ops.maxLag = ops.max_lag;
 OAK='/oak/stanford/groups/giocomo/';
 %OAK = '/Volumes/Samsung_T5';
+gains = [0.5, 0.6, 0.7, 0.8];
 %%
-gain = 0.6;
+gain = 0.6; 
 contrast = 100;
 regions = {'MEC'};
 filenames = {};
@@ -24,6 +25,7 @@ for iR = 1:numel(regions)
     triggers = cat(2,triggers,tmp2);
 end
 savepath = fullfile(OAK,'attialex','tbtxcorr_range');
+fig_save_path = fullfile(savepath,'images');
 shiftDir = fullfile(OAK,'attialex','speed_filtered_correctedData');
 if ~isfolder(savepath)
     mkdir(savepath)
@@ -37,59 +39,106 @@ if isempty(p)
     p = parpool(12);
 end
 %%
-parfor iF=1:numel(filenames)
+fig=figure('Renderer','Painters','Position',[440   611   877   187],'Visible','off');
+cmap = cbrewer('seq','BuPu',20);
+
+colormap(cmap)
+%%
+for iF=1:numel(filenames)
     
     try
         [~,sn]=fileparts(filenames{iF});
+        mkdir(fullfile(fig_save_path,sn))
         
         
-            data = load(filenames{iF});
-
-            if isfield(data.anatomy,'parent_shifted')
-                reg = data.anatomy.parent_shifted;
-            else
-                reg = data.anatomy.cluster_parent;
+        data = load(filenames{iF});
+        
+        if isfield(data.anatomy,'parent_shifted')
+            reg = data.anatomy.parent_shifted;
+        else
+            reg = data.anatomy.cluster_parent;
+        end
+        if iscolumn(reg)
+            reg = reg';
+        end
+        
+        reg=reg(data.sp.cgs==2);
+        reg_orig = data.anatomy.cluster_parent((data.sp.cgs==2));
+        if iscolumn(reg_orig)
+            reg_orig = reg_orig';
+        end
+        
+        
+        
+        %calculate trial by trial correlation across all bins
+        trials=1:max(data.trial);
+        ops_here = ops;
+        ops_here.trials = trials;
+        cellID = data.sp.cids(data.sp.cgs==2);
+        
+        [corrMat,frMat,shiftMat]=trialCorrMat(cellID,trials,data,ops);
+        
+        for iC=1:size(frMat,1)
+            good=0;
+            for iG=1:numel(gains)
+                onsets = strfind(data.trial_gain' == gains(iG),[0 1])+1;
+                pre_map = zeros(2,200);
+                pre_map_all = zeros(2,4,200);
+                gain_map_all = pre_map_all;
+                gain_map = pre_map;
+                pre_stab = zeros(1,2);
+                for iO=1:2
+                    onset = onsets(iO);
+                    pre_idx = onset-4:onset-1;
+                    gain_idx = onset+(0:3);
+                    pre_map_all(iO,:,:)=squeeze(frMat(iC,pre_idx,:));
+                    gain_map_all(iO,:,:)=squeeze(frMat(iC,gain_idx,:));
+                    pre_map(iO,:) = squeeze(mean(frMat(iC,pre_idx,:),2));
+                    gain_map(iO,:)=squeeze(mean(frMat(iC,gain_idx,:),2));
+                    pre_stab(iO) = nanmean(nanmean(corrMat(iC,pre_idx,pre_idx),2),3);
+                end
+                
+                if all(pre_stab>.3)
+                    good=good+1;
+                    subplot(2,4,iG)
+                    imagesc([pre_map(1,:);gain_map(1,:);pre_map(2,:);gain_map(2,:)])
+                    subplot(2,4,iG+4)
+                    imagesc([squeeze(pre_map_all(1,:,:));squeeze(gain_map_all(1,:,:));squeeze(pre_map_all(2,:,:));squeeze(gain_map_all(2,:,:))])
+                    
+                end
+                
             end
-            if iscolumn(reg)
-                reg = reg';
+            %colormap(cmap)
+            
+            %pause
+            if good>3
+                cluID = cellID(iC);
+                saveas(fig,fullfile(fig_save_path,sn,sprintf('%d.png',cluID)))
             end
+            clf
             
-            reg=reg(data.sp.cgs==2);
-            reg_orig = data.anatomy.cluster_parent((data.sp.cgs==2));
-            if iscolumn(reg_orig)
-                reg_orig = reg_orig';
-            end
-            
-            
-            
-            %calculate trial by trial correlation across all bins
-            trials=1:max(data.trial);
-            ops_here = ops;
-            ops_here.trials = trials;
-            cellID = data.sp.cids(data.sp.cgs==2);
-            
-            [corrMat,b,shiftMat]=trialCorrMat(cellID,trials,data,ops);
-            
-            
-            
-            
-            data_out = matfile(fullfile(savepath,sprintf('%s',sn)),'Writable',true);
-            data_out.corrMat = corrMat;
-            data_out.shiftMat = shiftMat;
-            
-            
-            
-            data_out.region = reg;
-     
-            
-            
-            data_out.trials = trials;
-            data_out.gain = data.trial_gain;
+        end
+        
+        
+        
+        
+        data_out = matfile(fullfile(savepath,sprintf('%s',sn)),'Writable',true);
+        data_out.corrMat = corrMat;
+        data_out.shiftMat = shiftMat;
+        
+        
+        
+        data_out.region = reg;
+        
+        
+        
+        data_out.trials = trials;
+        data_out.gain = data.trial_gain;
         
     catch ME
         disp(ME.message)
         disp(sprintf('filenr: %d',iF))
-        rethrow(ME)
+        %rethrow(ME)
     end
 end
 
