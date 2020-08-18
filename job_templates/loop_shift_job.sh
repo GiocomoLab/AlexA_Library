6c043eb4120fe44059643ee8b584ed2af234d4b2
alex-attinger <a.attinger@gmail.com>
2020-08-17 17:35:01 -0700

    Update loop_shift_job.sh

--- job_templates/loop_shift_job.sh
+++ job_templates/loop_shift_job.sh
@@ -13,4 +13,4 @@
 module load matlab
 echo "$(date): job $SLURM_JOBID starting on $SLURM_NODELIST"
 
-matlab -nodisplay -nosplash -r "run $HOME/AlexA_Library/NeuroPixel/speed_sorting/loop_findshifts_v2.m, exit"
+matlab -nodisplay -nosplash -r "run $HOME/startup.m,$HOME/AlexA_Library/NeuroPixel/speed_sorting/loop_findshifts_v2.m,exit"
