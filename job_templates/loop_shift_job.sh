f9b256ce7922f9b41951693bc2312ca5af70cdb6
alex-attinger <a.attinger@gmail.com>
2020-08-24 11:08:13 -0700

    Update loop_shift_job.sh

--- job_templates/loop_shift_job.sh
+++ job_templates/loop_shift_job.sh
@@ -2,15 +2,15 @@
 #SBATCH -p giocomo
 #SBATCH --job-name=find_shifts
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=24
+#SBATCH --cpus-per-task=12
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=attialex@stanford.edu
 #SBATCH --mem-per-cpu=4G
-#SBATCH --time=12:00:00
+#SBATCH --time=4:00:00
 #SBATCH -o /scratch/users/attialex/reg.%N.%j.out # STDOUT
 #SBATCH -e /scratch/users/attialex/reg.%N.%j.err # STDERR
 
 module load matlab
 echo "$(date): job $SLURM_JOBID starting on $SLURM_NODELIST"
 cd $HOME
-matlab -nodisplay -nosplash -r "run ./AlexA_Library/NeuroPixel/speed_sorting/loop_findshifts.m,exit"
+matlab -nodisplay -nosplash -r "run ./AlexA_Library/NeuroPixel/speed_sorting/loop_xcorrv2.m,exit"
