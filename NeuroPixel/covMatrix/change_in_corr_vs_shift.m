0ebef192d6dcde53fc7d0ff383b0332fc4747c35
alex-attinger <a.attinger@gmail.com>
2020-08-31 11:47:15 -0700

    Update change_in_corr_vs_shift.m

--- NeuroPixel/covMatrix/change_in_corr_vs_shift.m
+++ NeuroPixel/covMatrix/change_in_corr_vs_shift.m
@@ -85,13 +85,16 @@ end
 %%
 
 %%
-for iJ=1:3
-pval=zeros(16,176);
+cluster_idx = CLUSTER_GROUP<3;
+pval_t = 0.05;
+
+for iJ=1:3:2
+pval=zeros(16,size(CORR_VEC,2));
 x1=pval;
-for iT=1:15
-delta_c = CORR_VEC(:,(iT-1)*nChunks+iJ)-CORR_VEC(:,iT*nChunks+iJ);
-for ii=1:size(CORR_VEC,2);
-    mdl = fitlm(SHIFT_VEC(:,ii),delta_c);
+for iT=1:16
+delta_c = CORR_VEC(cluster_idx,(iT-1)*nChunks+1)-CORR_VEC(cluster_idx,(iT-1)*nChunks+(nChunks-iJ));
+for ii=1:size(CORR_VEC,2)
+    mdl = fitlm(SHIFT_VEC(cluster_idx,ii),delta_c);
 
     pp=anova(mdl);
     pval(iT,ii) = pp{1,5};
@@ -102,25 +105,25 @@ end
 
 figure
 subplot(1,3,1)
-imagesc(pval<0.05)
+imagesc(pval,[0 0.05])
 hold on
 for ii=1:16
-xline(ii*11,'w')
+xline(ii*nChunks,'w')
 end
 subplot(1,3,2)
-imagesc(x1,[-.01 .01])
+imagesc(pval<pval_t)
 hold on
 for ii=1:16
-xline(ii*11,'w')
+xline(ii*nChunks,'w')
 end
 
 subplot(1,3,3)
 tmp = x1;
-tmp(pval>0.05)=0;
+tmp(pval>pval_t)=0;
 imagesc(tmp,[-.01 .01])
 hold on
 for ii=1:16
-xline(ii*11,'w')
+xline(ii*nChunks,'w')
 end
 end
 %%
@@ -143,10 +146,11 @@ for ii=1:size(CV,2)
 end
 end
 %%
-pval_t = 0.01;
+pval_t = 0.05/size(CV,2);
 figure
 subplot(2,1,1)
-imagesc(pval(7,:)<pval_t)
+trial = 6
+imagesc(pval(trial,:)<pval_t)
 hold on
 for ii=1:16
 xline(ii*nChunks,'w')
@@ -164,10 +168,10 @@ set(gca,'XTick',[nChunks/2:nChunks:size(CORR_VEC,2)],'XTickLabel',[1:16])
 subplot(2,1,2)
 tmp = x1;
 tmp(pval>pval_t)=0;
-imagesc(tmp(7,:),[-.01 .01])
+imagesc(tmp(trial,:),[-.01 .01])
 hold on
 for ii=1:16
-xline(ii*11,'w')
+xline(ii*nChunks,'w')
 end
 set(gca,'XTick',[nChunks/2:nChunks:size(CORR_VEC,2)],'XTickLabel',[1:16])
 title('slope in sig. models')
@@ -194,9 +198,9 @@ SC= SHIFT_CLASSIC(cluster_idx,:);
 STABLE = SHIFT_CLASSIC(~cluster_idx,:);
 %delta_c = nanmean(nanmean(CORR_MAT(:,1:6,7:10),2),3);
 %delta_c = delta_c./mean(CORR_VEC(:,1:66),2);
-slices = 2;
+slices = 3;
 data2compare = {};
-CT=cbrewer('seq', 'Blues', slices+2);
+CT=cbrewer('qual', 'Set2', slices);
 N=floor(numel(delta_c)/slices);
 [a,b]=sort(delta_c);
 figure('Renderer','Painters')
@@ -209,16 +213,16 @@ tmp_s = nanmean(SC(idx,:));
 data2compare{iS}=SV(idx,take_idx);
 subplot(11,1,[1:5])
 hold on
-boundedline(x_vec(take_idx),tmp_corr,std(CV(idx,take_idx))/sqrt(nnz(idx)),'cmap',CT(iS+2,:),'alpha')
+boundedline(x_vec(take_idx),tmp_corr,std(CV(idx,take_idx))/sqrt(nnz(idx)),'cmap',CT(iS,:),'alpha')
 
 %plot(x_vec,tmp_corr,'Color',CT(iS+2,:))
 subplot(11,1,[6:10])
 hold on
-boundedline(x_vec(take_idx),tmp_shift,std(SV(idx,take_idx))/sqrt(nnz(idx)),'cmap',CT(iS+2,:),'alpha')
-plot(x_vec(floor(nChunks/2):nChunks:end),tmp_s*-1,'Color',CT(iS+2,:),'LineWidth',2)
+boundedline(x_vec(take_idx),tmp_shift,std(SV(idx,take_idx))/sqrt(nnz(idx)),'cmap',CT(iS,:),'alpha')
+%plot(x_vec(floor(nChunks/2):nChunks:end),tmp_s*-1,'Color',CT(iS,:),'LineWidth',2)
 %lot(x_vec,tmp_shift,'Color',CT(iS+2,:))
 end
-plot(x_vec(floor(nChunks/2):nChunks:end),mean(STABLE)*-1,'k--','LineWidth',2)
+%plot(x_vec(floor(nChunks/2):nChunks:end),mean(STABLE)*-1,'k--','LineWidth',2)
 
 subplot(11,1,[1:5])
 for ii=1:4
@@ -244,6 +248,85 @@ subplot(11,1,11)
 imagesc(pvals<0.025)
 box off
 %saveas(gcf,'/Users/attialex/Dropbox/temporary_images/fig6_shiftvsgain.pdf')
+%% looking at those that remain stable in trial 1
+x_vec=zeros(nChunks,16);
+for ii=1:16
+x_vec(:,ii)=linspace(100,300,nChunks)+(ii-1)*400;
+
+end
+
+take_idx=1:16*nChunks;
+x_vec = reshape(x_vec,1,[]);
+cluster_idx = CLUSTER_GROUP<3;
+% delta_c_1 = CORR_VEC(:,(iT-1)*nChunks+1)-CORR_VEC(:,(iT-1)*nChunks+nChunks);
+% delta_c_1 = delta_c_1(cluster_idx);
+gain_1_stab = nanmean(nanmean(CORR_MAT(:,1:6,7),2),3);
+ranking = 1:numel(gain_1_stab);
+[~,tmpsid]=sort(gain_1_stab);
+ranking(tmpsid)=ranking;
+ranking = ranking/max(ranking);
+iT=8;
+delta_c = CORR_VEC(:,(iT-1)*nChunks+1)-CORR_VEC(:,(iT-1)*nChunks+10);
+
+selection_idx = cluster_idx & ranking'>0.5;
+delta_c = delta_c(selection_idx);
+
+CV = CORR_VEC(selection_idx,:);
+SV = SHIFT_VEC(selection_idx,:);
+SC= SHIFT_CLASSIC(selection_idx,:);
+%delta_c = nanmean(nanmean(CORR_MAT(:,1:6,7:10),2),3);
+%delta_c = delta_c./mean(CORR_VEC(:,1:66),2);
+slices = 2;
+data2compare = {};
+CT=cbrewer('qual', 'Set1', slices);
+N=floor(numel(delta_c)/slices);
+[a,b]=sort(delta_c);
+figure('Renderer','Painters')
+for iS=1:slices
+idx = (iS-1)*N+1:iS*N;
+idx=b(idx);
+tmp_corr = mean(CV(idx,take_idx));
+tmp_shift = mean(SV(idx,take_idx));
+tmp_s = nanmean(SC(idx,:));
+data2compare{iS}=SV(idx,take_idx);
+subplot(11,1,[1:5])
+hold on
+boundedline(x_vec(take_idx),tmp_corr,std(CV(idx,take_idx))/sqrt(nnz(idx)),'cmap',CT(iS,:),'alpha')
+
+%plot(x_vec,tmp_corr,'Color',CT(iS+2,:))
+subplot(11,1,[6:10])
+hold on
+boundedline(x_vec(take_idx),tmp_shift,std(SV(idx,take_idx))/sqrt(nnz(idx)),'cmap',CT(iS,:),'alpha')
+%plot(x_vec(floor(nChunks/2):nChunks:end),tmp_s*-1,'Color',CT(iS+2,:),'LineWidth',2)
+%lot(x_vec,tmp_shift,'Color',CT(iS+2,:))
+end
+
+subplot(11,1,[1:5])
+for ii=1:4
+xline((5+ii)*400+1)
+end
+title('XCorrPeak')
+legend({'small','large'})
+axis tight
+box off
+subplot(11,1,[6:10])
+for ii=1:4
+xline((5+ii)*400+1)
+end
+legend({'small','large'})
+title('Shift')
+axis tight
+box off
+pvals = zeros(1,numel(take_idx));
+for ii=1:numel(take_idx)
+    pvals(ii)=ranksum(data2compare{1}(:,ii),data2compare{2}(:,ii),'tail','left');
+end
+subplot(11,1,11)
+imagesc(pvals<0.025)
+box off
+%saveas(gcf,'/Users/attialex/Dropbox/temporary_images/fig6_shiftvsgain.pdf')
+
+
 %%
 idx = diag(true(1,15),1);
 d1=CORR_MAT(:,idx);
