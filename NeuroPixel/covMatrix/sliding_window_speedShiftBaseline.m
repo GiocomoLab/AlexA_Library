6b321a6fa85e805877cf9c8755e3c6a79f87d945
alex-attinger <a.attinger@gmail.com>
2020-08-31 11:55:42 -0700

    Update sliding_window_speedShiftBaseline.m

--- NeuroPixel/covMatrix/sliding_window_speedShiftBaseline.m
+++ NeuroPixel/covMatrix/sliding_window_speedShiftBaseline.m
@@ -7,13 +7,11 @@ SPEED = [];
 REG={};
 CLU=[];
 FID = [];
+SESSION = {};
 for iF=1:numel(matfiles)
     
     [~,sn]=fileparts(matfiles(iF).name);
     parts = strsplit(sn,'_');
-    if str2double(parts{end})>5
-        continue
-    end
     
     
     data_out = load(fullfile(matfiles(iF).folder,matfiles(iF).name));
@@ -32,10 +30,12 @@ for iF=1:numel(matfiles)
     if nnz(valid_idx)<5
         continue
     end
+    parts = strsplit(sn,'_');
     reg=reg(valid_idx);
     %PEAKS = cat(1,PEAKS,data_out.PEAKS(valid_idx,:,:));
     PEAKS{end+1}=data_out.PEAKS(valid_idx,:,:);
     SHIFTS{end+1}=data_out.SHIFTS(valid_idx,:,:);
+    SESSION{end+1}=strcat(parts{1},'_',parts{2});
     %SHIFTS = cat(1,SHIFTS,data_out.SHIFTS(valid_idx,:,:));
     SPEED = cat(3,SPEED,data_out.SPEED2);
     REG = cat(1,REG,reg(1));
@@ -147,4 +147,79 @@ end
     ylabel('estimated delay')
     xlabel('Position')
     
+%% average over reps and sessions
+SESSION_DATA=struct();
+SESSION_SPEED=struct();
+for iR=1:3
+    sel_idx = (startsWith(REG,regions{iR}));
+    SHIFTS_THIS = SHIFTS(sel_idx);
+    SESSION_THIS = SESSION(sel_idx);
+    SPEED_THIS = SPEED(:,:,sel_idx);
+    
+    [uS,~,idx]=unique(SESSION_THIS);
+    SESSION_DATA.(regions{iR})=cell(numel(uS),1);
+    for iS=1:numel(uS)
+        sel_idx = find(idx==iS);
+
+    FASTEST=zeros(nnz(sel_idx),nChunks);
+        SLOWEST = FASTEST;
+        Speed_diff = FASTEST;
+
+        for iRep=1:numel(sel_idx)
+            sp=SPEED_THIS(:,:,sel_idx(iRep));
+            [ma,fastest]=max(sp);
+            [mi,slowest]=min(sp);
+            Speed_diff(iRep,:)= ma-mi;
+
+            p1=zeros(1,nChunks);
+            p2=p1;
+            for ii=1:nChunks
+                FASTEST(iRep,ii)=nanmean(SHIFTS_THIS{sel_idx(iRep)}(:,fastest(ii),ii));
+                SLOWEST(iRep,ii)=nanmean(SHIFTS_THIS{sel_idx(iRep)}(:,slowest(ii),ii));
+            end
+            
+        end
+        SESSION_DATA.(regions{iR}){iS}=cat(3,FASTEST,SLOWEST);
+        SESSION_SPEED.(regions{iR}){iS}=Speed_diff;
+    end
+    
+end
+
 %%
+tracefig =figure();
+for iR=1:3
+    data_this =SESSION_DATA.(regions{iR});
+    av_fast = [];
+    av_slow = [];
+    av_speed = [];
+    for iS=1:size(data_this,1)
+        if size(data_this{iS},1)<2
+            continue
+        end
+        av_fast=cat(1,av_fast,squeeze(nanmean(data_this{iS}(:,:,1))));
+        av_slow=cat(1,av_slow,squeeze(nanmean(data_this{iS}(:,:,2))));
+        av_speed = cat(1,av_speed,squeeze(nanmean(SESSION_SPEED.(regions{iR}){iS})));
+    end
+    figure(tracefig);
+        subplot(2,3,iR)
+
+    boundedline(x_vec,nanmean(av_fast),nanstd(av_fast)/sqrt(size(av_fast,1)),'alpha','cmap',[1 0 0])
+    boundedline(x_vec,nanmean(av_slow),nanstd(av_slow)/sqrt(size(av_slow,1)),'alpha','cmap',[0 0 1])
+        ylabel('map shift')
+    
+    ylim([-2 2])
+        yyaxis right
+        boundedline(x_vec,nanmean(av_speed),nanstd(av_speed)/sqrt(size(av_speed,1)),'alpha','cmap',[.5 .5 .5])
+    xlabel('Position')
+    title(regions{iR})
+    subplot(2,3,iR+3)
+    plot(x_vec,av_fast'-av_slow','b')
+    
+        ylim([-10 10])
+        tmp=av_fast-av_slow;
+        tmps = nanmean(tmp,2);
+        tmpd=nanmean(av_speed,2);
+    DELAYS{iR}=tmps./tmpd;
+end
+        figure;plotSpread(DELAYS)
+        xticklabels(regions)
