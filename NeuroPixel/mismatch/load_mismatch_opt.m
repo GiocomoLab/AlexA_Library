c16403645840f916f0379165a19d484011cb3bc6
alex-attinger <a.attinger@gmail.com>
2020-09-06 19:29:28 -0700

    Update load_mismatch_opt.m

--- NeuroPixel/mismatch/load_mismatch_opt.m
+++ NeuroPixel/mismatch/load_mismatch_opt.m
@@ -2,3 +2,11 @@ function opt = load_mismatch_opt()
 
 opt = struct();
 opt.speed_t=0.05;
+opt.extract_win = [-2 3];
+opt.aux_win = [-50 50];
+opt.TimeBin = 0.02;
+opt.time_bins =-2:0.02:3;
+opt.time_vecs = opt.time_bins(1:end-1)*0.5+opt.time_bins(2:end)*0.5;
+opt.extract_win = [-2 3];
+opt.aux_win = [-50 50];
+opt.TimeBin = 0.02;
