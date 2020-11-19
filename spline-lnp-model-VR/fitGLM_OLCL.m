c5617ab424d8f9f80881d5315e7fa14edb4dab12
alex-attinger <a.attinger@gmail.com>
2020-11-19 14:05:00 -0800

    updating glm code

---
+++ spline-lnp-model-VR/fitGLM_OLCL.m
@@ -0,0 +1,182 @@
+function [glmData] = fitGLM_OLCL(dataOL,dataCL,good_cells,downsample)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+%% for OL, used to fit model
+
+opt = load_mismatch_opt;
+velM = preprocessSignal(dataOL.vr_data_resampled.velM',21);
+velP = preprocessSignal(dataOL.vr_data_resampled.velP',21);
+pa = removeOutliers(dataOL.pupil_area,[1 99]);
+pupil_area = preprocessSignal(pa,11);
+
+p_x = removeOutliers(dataOL.pupil_com(:,1),[1 99]);
+pupil_x = preprocessSignal(p_x,11);
+p_y = removeOutliers(dataOL.pupil_com(:,2),[1 99]);
+pupil_y = preprocessSignal(p_y,11);
+n_OL = numel(velM);
+%% CL, only create design matrix to use for prediction
+
+velM_cl = preprocessSignal(dataCL.vr_data_resampled.velM',21);
+velP_cl = preprocessSignal(dataCL.vr_data_resampled.velP',21);
+pa = removeOutliers(dataCL.pupil_area,[1 99]);
+pupil_area_cl = preprocessSignal(pa,11);
+
+p_x = removeOutliers(dataCL.pupil_com(:,1),[1 99]);
+pupil_x_cl = preprocessSignal(p_x,11);
+p_y = removeOutliers(dataCL.pupil_com(:,2),[1 99]);
+pupil_y_cl = preprocessSignal(p_y,11);
+
+%%
+
+
+
+% take position mod length of track (AFTER computing speed)
+
+
+glmData=struct();
+sample_idx = 1:downsample:numel(velM);
+%% create A
+
+var_name = {'velM','velP','pupilArea','pupilX','pupilY'};
+A={};
+s = 0.5; % spline parameter
+
+%% velM and velP
+spdVec = linspace(min([velM; velP;velM_cl;]),max([velM; velP; velM_cl]),15);
+spdVec(1)=spdVec(1)-0.1;
+%spdVec(1)=-0.01; %for boundary condition
+%maybe use linspace?
+[velMgrid,ctl_pts_velM] = spline_1d([velM;velM_cl],spdVec,s);
+[velPgrid,ctl_pts_velP] = spline_1d([velP; velP_cl],spdVec,s);
+
+
+A{1} = velMgrid(1:n_OL,:);
+B{1} = velMgrid(n_OL+1:end,:);
+all_control_points{1} = ctl_pts_velM;
+A{2} = velPgrid(1:n_OL,:);
+B{2} = velPgrid(n_OL+1:end,:)
+all_control_points{2} = ctl_pts_velP;
+%% pupil
+areaVec = linspace(min([pupil_area;pupil_area_cl]),max([pupil_area;pupil_area_cl]),11);
+areaVec(1)=[areaVec(1)-0.01];
+areaVec(end)=areaVec(end)+0.01;
+[areaGrid,ctl_pts_area] = spline_1d([pupil_area;pupil_area_cl],areaVec,s);
+
+xVec = linspace(min([pupil_x;pupil_x_cl]),max([pupil_x;pupil_x_cl]),11);
+xVec(1)=xVec(1)-0.01;
+xVec(end)=xVec(end)+0.01;
+[xGrid,ctl_pts_x] = spline_1d([pupil_x;pupil_x_cl],xVec,s);
+
+yVec = linspace(min([pupil_y;pupil_y_cl]),max([pupil_y;pupil_y_cl]),11);
+yVec(1)=yVec(1)-0.01;
+yVec(end)=yVec(end)+0.01;
+[yGrid,ctl_pts_y] = spline_1d([pupil_y;pupil_y_cl],yVec,s);
+
+A{3}=areaGrid(1:n_OL,:);
+B{3}=areaGrid(n_OL+1:end,:);
+A{4}=xGrid(1:n_OL,:);
+B{4}=xGrid(n_OL+1:end,:);
+A{5}=yGrid(1:n_OL,:);
+B{5}=yGrid(n_OL+1:end,:);
+all_control_points{3} = ctl_pts_area;
+all_control_points{4} = ctl_pts_x;
+all_control_points{5} = ctl_pts_y;
+%% MM
+ff = zeros(size(dataOL.vr_data_resampled.MM,2),4);
+for ii=1:4
+    ff(:,ii)=circshift(dataOL.vr_data_resampled.MM,(ii-1)*5); 
+end
+
+A{6}=ff;
+
+%%
+for iA=1:numel(A)
+    A{iA}=A{iA}(sample_idx,:);
+end
+%% MM
+%A{3}=bl_trial;
+
+%%
+for cellIDX=1:length(good_cells)
+    spike_t = dataOL.sp.st(dataOL.sp.clu==good_cells(cellIDX));
+    %[~,~,spike_idx] = histcounts(spike_t,post);
+    
+    [spiketrain,~,spikeIDX] = histcounts(spike_t,dataOL.post(sample_idx));
+    spiketrain = spiketrain';
+    numFolds = 10;
+    T = numel(spiketrain);
+    numPts = 10*round(1/opt.TimeBin); % 3 seconds. i've tried #'s from 1-10 seconds.. not sure what is best
+    for iA=1:numel(A)
+        A{iA}=A{iA}(1:T,:);
+    end
+    
+    %%
+    [train_ind,test_ind] = compute_test_train_ind(numFolds,numPts,T);
+    %     trial = dataOL.trial(sample_idx);
+    %     tmp_ind = mod(trial(1:T),numFolds);
+    %     for k = 1:numFolds
+    %         test_ind{k} = find(tmp_ind == k-1);
+    %         train_ind{k} = setdiff(1:T,test_ind{k});
+    %     end
+    %%%%%%%% FORWARD SEARCH PROCEDURE %%%%%%%%%
+    try
+        fprintf('\t Fitting model  for cell %d \n', cellIDX);
+        [allModelTestFits, allModelTrainFits, bestModels, bestModelFits, parameters, pvals, final_pval] = forward_search_kfold(A,spiketrain,train_ind,test_ind);
+        
+        vars = sort(bestModels);
+        X=ones(length(spiketrain),1);
+        for iV=vars
+            X=[X A{iV}];
+        end
+        yhat = exp(X*parameters{end}');
+        
+        
+    catch ME
+        fprintf('Model fitting failed for %d \n',cellIDX)
+        warning(ME.message)
+        allModelTestFits = NaN;
+        bestModels = NaN;
+        bestModelFits = NaN;
+        parameters = NaN;
+        pvals = [];
+        tuning_curves ={};
+        final_pval = nan;
+        yhat =[];
+    end
+    try
+        tuning_curves = glm_tuning_curves_noPos(A,bestModels,parameters{end},all_control_points,s,median(diff(dataOL.post(sample_idx))));
+    catch ME
+        fprintf('Tuning curves failed \n')
+    end
+    
+    
+    
+    
+    
+    glmData(cellIDX).allModelTestFits = allModelTestFits;
+    glmData(cellIDX).bestModels = bestModels;
+    glmData(cellIDX).bestModelFits = bestModelFits;
+    glmData(cellIDX).parameters = parameters;
+    glmData(cellIDX).pvals = pvals;
+    glmData(cellIDX).yhat = yhat;
+    glmData(cellIDX).var_name = var_name;
+    glmData(cellIDX).final_pval = final_pval;
+    glmData(cellIDX).tuning_curves  = tuning_curves;
+    
+end
+
+function signal = preprocessSignal(signal,smoothWin)
+signal = fillmissing(signal,'nearest');
+fi = gausswin(smoothWin);
+fi = fi/sum(fi);
+signal = conv([repmat(signal(1),floor(smoothWin/2),1); signal; repmat(signal(end),floor(smoothWin/2),1)],fi,'valid');
+
+
+function signal = removeOutliers(signal,quantiles)
+quants=prctile(signal,quantiles);
+mi = quants(1);
+ma = quants(2);
+signal(signal<mi)=mi;
+signal(signal>ma)=ma;
+
+
--- spline-lnp-model-VR/fitGLM_mismatch.m
+++ spline-lnp-model-VR/fitGLM_mismatch.m
@@ -1,4 +1,4 @@
-function [glmData] = fitGLM_mismatch(data)
+function [glmData] = fitGLM_mismatch(data,good_cells,downsample)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -6,10 +6,14 @@ function [glmData] = fitGLM_mismatch(data)
 
 
 
-   
-    %bl_trial=data.trial(bl_idx);
-    params.TimeBin = 0.02;
+
+%bl_trial=data.trial(bl_idx);
+params.TimeBin = 0.02;
+if isfield(data,'true_speed')
     speed=data.true_speed;
+else
+    speed = calcSpeed(data.posx,load_default_opt);
+end
 speed(speed<0) = 0;
 max_speed=ceil(prctile(speed,99));
 speed(speed > max_speed) = max_speed;
@@ -20,6 +24,7 @@ posx(posx>400)=400;
 
 glmData=struct();
 keep_data={};
+sample_idx = 1:downsample:numel(posx);
 %% create A
 
 var_name = {'position','speed'};
@@ -42,29 +47,40 @@ spdVec(1)=-0.01; %for boundary condition
 [speedgrid,ctl_pts_speed] = spline_1d(speed(2:end),spdVec,s);
 A{2} = speedgrid;
 all_control_points{2} = ctl_pts_speed;
+
+for iA=1:numel(A)
+    A{iA}=A{iA}(sample_idx,:);
+end
 %% gain
 %A{3}=bl_trial;
 
 %%
-good_cells = data.sp.cids(data.sp.cgs==2);
-for cellIDX=17:length(good_cells)
+for cellIDX=1:length(good_cells)
     spike_t = data.sp.st(data.sp.clu==good_cells(cellIDX));
     %[~,~,spike_idx] = histcounts(spike_t,post);
     
-    spiketrain = histcounts(spike_t,data.post)';
+    [spiketrain,~,spikeIDX] = histcounts(spike_t,data.post(sample_idx));
+    spiketrain = spiketrain';
     numFolds = 10;
     T = numel(spiketrain);
     numPts = 3*round(1/params.TimeBin); % 3 seconds. i've tried #'s from 1-10 seconds.. not sure what is best
-    
+    for iA=1:numel(A)
+    A{iA}=A{iA}(1:T,:);
+end
     
     %%
     [train_ind,test_ind] = compute_test_train_ind(numFolds,numPts,T);
-    
+    trial = data.trial(sample_idx);
+    tmp_ind = mod(trial(1:T),numFolds);
+    for k = 1:numFolds
+        test_ind{k} = find(tmp_ind == k-1);
+        train_ind{k} = setdiff(1:T,test_ind{k});
+    end
     %%%%%%%% FORWARD SEARCH PROCEDURE %%%%%%%%%
-    try 
+    try
         fprintf('\t Fitting model  for cell %d \n', cellIDX);
         [allModelTestFits, allModelTrainFits, bestModels, bestModelFits, parameters, pvals, final_pval] = forward_search_kfold(A,spiketrain,train_ind,test_ind);
-
+        
     catch ME
         fprintf('Model fitting failed for %d \n',cellIDX)
         warning(ME.message)
@@ -77,11 +93,11 @@ for cellIDX=17:length(good_cells)
         final_pval = nan;
     end
     try
-        tuning_curves = glm_tuning_curves(A,bestModels,parameters{end},all_control_points,s,0.02);
+        tuning_curves = glm_tuning_curves(A,bestModels,parameters{end},all_control_points,s,median(diff(data.post(sample_idx))));
     catch ME
         fprintf('Tuning curves failed \n')
     end
-
+    
     glmData(cellIDX).allModelTestFits = allModelTestFits;
     glmData(cellIDX).bestModels = bestModels;
     glmData(cellIDX).bestModelFits = bestModelFits;
@@ -91,6 +107,6 @@ for cellIDX=17:length(good_cells)
     glmData(cellIDX).var_name = var_name;
     glmData(cellIDX).final_pval = final_pval;
     glmData(cellIDX).tuning_curves  = tuning_curves;
-
+    
 end
 
