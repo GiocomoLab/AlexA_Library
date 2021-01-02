feb2838020a970ba8c25847b5378686b7ccfc35b
alex-attinger <a.attinger@gmail.com>
2021-01-02 14:22:46 -0800

    Create spikeStruct2countVec.m

---
+++ NeuroPixel/helpers/spikeStruct2countVec.m
@@ -0,0 +1,13 @@
+function count_vec = spikeStruct2countVec(trial_vec, good_cells,opt)
+%trial_vec =cat(1,spike_times_struct{:});
+count_vec = zeros(numel(good_cells),numel(opt.time_bins)-1);
+for iC=1:numel(good_cells)
+    idx = trial_vec(:,2)==good_cells(iC);
+    [spike_count]=histcounts(trial_vec(idx,1),opt.time_bins);
+    count_vec(iC,:)=spike_count;
+    
+end
+
+
+n_trigs_included = numel(unique(trial_vec(:,3)));
+count_vec = count_vec/n_trigs_included/opt.TimeBin;
