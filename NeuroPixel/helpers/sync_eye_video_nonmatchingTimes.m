69a2c059a4bfbd68b7cda31bec33417cf45a7b8e
alex-attinger <a.attinger@gmail.com>
2021-02-20 16:41:33 -0800

    Update sync_eye_video_nonmatchingTimes.m

--- NeuroPixel/helpers/sync_eye_video_nonmatchingTimes.m
+++ NeuroPixel/helpers/sync_eye_video_nonmatchingTimes.m
@@ -13,8 +13,17 @@ for iF=1:numel(matfiles)
     ft_file = fullfile(vid_folder,[sn '.ft']);
     ft_data = importdata(ft_file);
     vid_data = load(vid_file);
-    pupil_area = vid_data.pupil{1}.area_smooth;
-    pupil_com = vid_data.pupil{1}.com_smooth;
+    tmp_file = strrep(vid_file,'_proc','_proc_area');
+    if isfile(tmp_file)
+        p=load(tmp_file);
+        area_raw = p.area;
+    else
+        fprintf('no raw area for %s \n',sn)
+        area_raw = nan(size(vid_data.pupil{1}.area));
+    end
+    pupil_area = vid_data.pupil{1}.area;
+    pupil_com = vid_data.pupil{1}.com;
+    pupil_area_raw = area_raw;
     vid_length = ft_data(end,2)-ft_data(1,2);
     
     %data = load(fullfile(matfiles(iF).folder,matfiles(iF).name));
@@ -30,26 +39,11 @@ for iF=1:numel(matfiles)
     vid_frame_times =  ft_data(:,2)-ft_data(1,2)+(-1*time_diff*0.5); %transforming vid_frame times by adding half of the difference between vr and video 
     pupil_area = interp1(vid_frame_times,pupil_area,data.post);
     pupil_com = interp1(vid_frame_times,pupil_com,data.post);
-    save(fullfile(matfiles(iF).folder,matfiles(iF).name),'pupil_area','pupil_com','-append')
-    save(fullfile('F:\Alex\matfiles_new',matfiles(iF).name),'pupil_area','pupil_com','-append')
+    pupil_area_raw = interp1(vid_frame_times,pupil_area_raw,data.post);
+    save(fullfile(matfiles(iF).folder,matfiles(iF).name),'pupil_area','pupil_com','pupil_area_raw','-append')
+    copyfile(fullfile(matfiles(iF).folder,matfiles(iF).name),'F:\Alex\matfiles_processed')
+    %save(fullfile('F:\Alex\matfiles_new',matfiles(iF).name),'pupil_area','pupil_com','-append')
 
 end
 %%
 
-
-%%
-figure
-h = animatedline;
-h.MaximumNumPoints=200
-h.LineWidth = 2;
-h.Color = [0 0 0]
-axis()
-hold on
-plot(x,y,'Color',[0.9,0.9,0.9],'LineWidth',0.1)
-x = vid_data.pupil{1}.com_smooth(:,1);
-y = vid_data.pupil{1}.com_smooth(:,2);
-for k = 1:length(x)
-    addpoints(h,x(k),y(k));
-    %head_1 = scatter(x(k),y(k),'ro');
-    drawnow
-end
