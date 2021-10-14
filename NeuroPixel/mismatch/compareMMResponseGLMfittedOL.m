d7fca33940ea33f4c603591943f706550f181dc7
alex-attinger <a.attinger@gmail.com>
2021-10-14 09:25:59 -0700

    Update compareMMResponseGLMfittedOL.m

--- NeuroPixel/mismatch/compareMMResponseGLMfittedOL.m
+++ NeuroPixel/mismatch/compareMMResponseGLMfittedOL.m
@@ -1,4 +1,6 @@
-matfiles = dir('F:\Alex\matfiles_new\*_PB_*.mat');
+%matfiles = dir('F:\Alex\matfiles_new\*_PB_*.mat');
+matfiles = dir('F:\Alex\new_2\*_PB_*.mat');
+
 
 for iF = 1:numel(matfiles)
     pb_file = matfiles(iF).name;
@@ -19,7 +21,7 @@ for iF = 1:numel(matfiles)
 end
 
 %%
-data_root = 'F:\Alex\matfiles_new'
+data_root = 'F:\Alex\new_2';
 files={
     {'AA_200919_1_PB_11-01-40.mat'       },    {'AA_200919_1_MM_10-47-17.mat'                          };
     {'AA_200919_1_PB_18-03-39.mat'       },    {'AA_200919_1_MM_17-41-26.mat'                          };
@@ -42,6 +44,28 @@ files={
     {'AA_200920_5_PB_201015_16-44-28.mat'},    {'AA_200920_5_MM_201015_16-29-02.mat'};
     {'AA_200920_5_PB_201018_11-34-12.mat'},    {'AA_200920_5_MM_201018_11-20-13.mat'};
     {'AA_200920_5_PB_201019_14-03-48.mat'},    {'AA_200920_5_MM_201019_13-46-40.mat'};
+    {'AA_201025_1_PB_201201_15-33-30.mat'     },    {'AA_201025_1_MM_201201_15-18-18.mat'     };
+    {'AA_201025_1_PB_201203_14-46-20.mat'     },   {'AA_201025_1_MM_201203_14-35-01.mat'     };
+    {'AA_201025_2_PB_201123_14-36-00.mat'     },    {'AA_201025_2_MM_201123_14-19-12.mat'     };
+    {'AA_201025_2_PB_201124_15-15-21.mat'     },    {'AA_201025_2_MM_201124_14-58-21.mat'     };
+    {'AA_201025_2_PB_201125_15-15-25.mat'     },    {'AA_201025_2_MM_201125_15-03-43.mat'     };
+    {'AA_201025_2_PB_201128_13-30-48.mat'     },    {'AA_201025_2_MM_201128_13-15-54.mat'     };
+    {'AA_201025_2_PB_201129_16-18-58.mat'     },    {'AA_201025_2_MM_201129_16-01-17.mat'     };
+    {'AA_201025_3_PB_201123_18-48-49.mat'     },    {'AA_201025_3_MM_201123_18-25-16.mat'     };
+    {'AA_201025_3_PB_201124_17-11-11.mat'     },    {'AA_201025_3_MM_201124_16-58-09.mat'     };
+    {'AA_201025_3_PB_201126_16-45-20.mat'     },    {'AA_201025_3_MM_201126_16-31-38.mat'     };
+    {'AA_201025_3_PB_201128_16-30-47.mat'     },    {'AA_201025_3_MM_201128_16-15-14.mat'     };
+    {'AA_201025_3_PB_201129_18-02-12.mat'     },    {'AA_201025_3_MM_201129_17-48-11.mat'     };
+    {'AA_201025_4_PB_201201_17-18-44.mat'     },    {'AA_201025_4_MM_201201_17-01-15.mat'     };
+    {'AA_201025_4_PB_201202_15-28-14.mat'     },    {'AA_201025_4_MM_201202_15-14-40.mat'     };
+    {'AA_210114_1_PB_210204_14-18-11_KS25.mat'},    {'AA_210114_1_MM_210204_14-08-49_KS25.mat'};
+    {'AA_210114_1_PB_210209_11-55-39_KS25.mat'},    {'AA_210114_1_MM_210209_11-45-12_KS25.mat'};
+    {'AA_210114_1_PB_210210_14-23-28_KS25.mat'},    {'AA_210114_1_MM_210210_14-10-20_KS25.mat'};
+    {'AA_210114_2_PB_210222_17-30-39_KS25.mat'},    {'AA_210114_2_MM_210222_17-12-25_KS25.mat'};
+%    {'AA_210114_3_PB_210204_17-02-29_KS25.mat'},    {'AA_210114_3_MM_210204_16-53-17.mat'     };
+%    {'AA_210114_3_PB_210209_14-53-33.mat'     },    {'AA_210114_3_MM_210209_14-40-56.mat'     };
+%    {'AA_210114_3_PB_210210_16-31-15.mat'     },    {'AA_210114_3_MM_210210_16-16-24.mat'     };
+    {'AA_210114_4_PB_210222_19-15-22_KS25.mat'},    {'AA_210114_4_MM_210222_19-02-07_KS25.mat'};
     };
 
 
@@ -51,8 +75,17 @@ files={
 for iF=1:size(files,1)
     
     data_cl = load(fullfile(data_root,files{iF,2}{1}));
+    if ~isfield(data_cl,'anatomy')
+        disp('no anatomy')
+        continue
+    end
     data_ol = load(fullfile(data_root,files{iF,1}{1}));
-    good_cells = data_ol.sp.cids(data_ol.sp.cgs==2);
+    %good_cells = data_ol.sp.cids(data_ol.sp.cgs==2);
+    good_cells = data_cl.sp.cids(data_cl.sp.cgs==2 & ismember(data_cl.sp.cids,data_cl.anatomy.cluster_id));
+    clu_histo_idx = ismember(data_cl.anatomy.cluster_id,good_cells);
+    
+    
+    region_this = data_cl.anatomy.cluster_parent(clu_histo_idx);
     try
     glmDataPB = fitGLM_OLCL(data_ol,data_cl,good_cells,1);
     
@@ -102,6 +135,7 @@ for iF=1:size(files,1)
     mf.glmData = glmDataPB;
     mf.mm_resp = count_vec;
     mf.mm_predicted = avg_yhat;
+    mf.region = region_this;
     figure;
     plot(opt.time_vecs,mean(count_vec))
     hold on
